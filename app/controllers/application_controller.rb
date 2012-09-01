219123569a8e3103e900d308c21021f138f70b72
Matías Battocchia <matias@riseup.net>
2012-09-01 04:42:03 -0300

    Separación de layouts externo (sign out) e interno (sign in) de la aplicación, y consecuente reestructuración de archivos; creación del layout interno.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+  layout :set_layout
+
   before_filter :set_locale
    
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
+
+  private
+    def set_layout
+      user_signed_in? ? 'inside' : 'outside'
+    end
 
 end
